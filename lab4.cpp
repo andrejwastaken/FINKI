3e762f76ebd3aaf3f017a1c69f2d83f8538ca6ab
andrejwastaken <60400035+andrejwastaken@users.noreply.github.com>
2023-11-16 11:23:51 +0100

    Add files via upload

---
+++ lab4.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+// 1
+// int n;
+// cin >> n;
+// int x;
+// int maks = 0;
+// int suma = 0;
+// for(int i=0; i <n; i++){
+//     cin >> x;
+//     int temp = x;
+//     suma=0;
+//     while(temp){
+//         suma+=temp%10;
+//         temp/=10;
+//     }
+//     if(suma%2==0&&x>maks)
+//         maks=x;
+// }
+// cout<<maks;
+
+// 2
+// int x;
+// cin>>x;
+// int brX=0;
+// while(x){
+//     brX++;
+//     x/=10;
+// }
+// int n, brN;
+// while(cin>>n){
+//     int temp = n;
+//     brN=0;
+//     while(temp){
+//         brN++;
+//         temp/=10;
+//     }
+//     if(brN==brX)
+//         cout<<n<<endl;
+// }
+// 3
+//    int a, b, palindrom=0, najmalP,najgolemP;
+//    cin >> a >> b;
+//    najmalP=b;
+//    najgolemP=a;
+//    for(int i = a; i<=b; i++){
+//        int temp = i;
+//        palindrom=0;
+//        while(temp){
+//            palindrom=palindrom*10+temp%10;
+//            temp/=10;
+//        }
+//        if(i<najmalP&&palindrom==i){
+//            najmalP=i;
+//        }
+//        if(i>najgolemP&&palindrom==i){
+//            najgolemP=i;
+//        }
+//    }
+//    cout<<najmalP<<endl;
+//    cout<<najgolemP;
+}
