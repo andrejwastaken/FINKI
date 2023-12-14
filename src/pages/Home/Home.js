00e03218aa78f1e87e88de4e2115267838fa39df
Filip Jordanoski <dzandarot@gmail.com>
2023-12-14 17:02:05 +0100

    Changes by Andrej

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../components/navbar/navbar';
 import Footer from '../../components/footer/footer';
+import Contact from '../../components/contact/contact';
 import './Home.css';
 import sliki from '../../assets/sliki'; // Import your assets
 
@@ -14,37 +15,6 @@ function Home(props) {
                     <button className="order-now-btn">ORDER NOW!</button>
                 </div>
 
-            </div>
-
-
-            <div class="location" id="loc1" >
-                <h3>Кеј</h3>
-                <p>Понеделник - Сабота
-                    <br></br>  10:00 - 00:00
-                    <br></br>   Недела
-                    <br></br>   10:00 - 23:30
-                    <br></br>   070/278-417
-                </p>
-            </div>
-
-            <div class="location" id="loc2">
-                <h3>Чаир</h3>
-                <p>Понеделник - Сабота
-                    <br></br>  10:00 - 00:00
-                    <br></br>  Недела
-                    <br></br>   10:00 - 23:30
-                    <br></br>   070/342-452 </p>
-
-            </div>
-
-            <div  class="location" id="loc3">
-                <h3 class="upper-line">Тетово</h3>
-                <p>Понеделник - Сабота
-                    <br></br>  10:00 - 00:00
-                    <br></br>  Недела
-                    <br></br>  10:00 - 23:30
-                    <br></br>  072/229-824
-                </p>
             </div>
 
             <Contact />
